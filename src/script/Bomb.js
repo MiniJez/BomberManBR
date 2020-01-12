2f2a58382bbd01d845f780d086acbd4a29e370ad
Edouard CLISSON <edouardclisson@gmail.com>
2020-01-12 15:37:54 +0100

    Fix: Bug death when behind a block with range upgrade

--- src/script/Bomb.js
+++ src/script/Bomb.js
@@ -7,21 +7,28 @@ const createBombs = (scene) => {
     return bombs
 }
 
-function playerIsDead(player, bomb, bonusBombRange){
+function playerIsDead(player, bomb, bonusUntilBlock, bonusBombRange){
     for (var i = 0; i <= bonusBombRange+1; i++){
-
         // left
         if((player.x < bomb.x && player.x > bomb.x - (28 + 32*i)) && (player.y > bomb.y - 28 && player.y < bomb.y + 28)) {
-            return true
+            if(i <= bonusUntilBlock[3]){
+                return true
+            }
         // right
         } else if((player.x > bomb.x && player.x < bomb.x + (28 + 32*i)) && (player.y > bomb.y - 28 && player.y < bomb.y + 28)) {
-            return true
+            if(i <= bonusUntilBlock[1]){
+                return true
+            }
         // bot
         } else if((player.y > bomb.y && player.y < bomb.y + (28 + 32*i)) && (player.x > bomb.x - 28 && player.x < bomb.x + 28)) {
-            return true
+            if(i <= bonusUntilBlock[2]){
+                return true
+            }
         // top
         } else if((player.y < bomb.y && player.y > bomb.y - (28 + 32*i)) && (player.x > bomb.x - 28 && player.x < bomb.x + 28)) {
-            return true
+            if(i <= bonusUntilBlock[0]){
+                return true
+            }
         // on 
         } else if(player.y == bomb.y && player.x == bomb.x) {
             return true
@@ -46,50 +53,56 @@ function loot(tile) {
 const bombExplose = (scene, bombs, bomb, map, player1, player2, playerType, bonusBombRange) => {
     creatBombParticles(scene, bomb, bonusBombRange)
 
+    //up, right, down, left = index 0, 1, 2, 3
+    bonusUntilBlock = [1, 1, 1, 1]
+
+
     for(var i = 1; i <= bonusBombRange+1; i++){
         tilesTop = map.getTilesWithinWorldXY(bomb.x - (TILE_SIZE / 2) , bomb.y - (TILE_SIZE * i + (TILE_SIZE / 2) ), TILE_SIZE, TILE_SIZE)
+        bonusUntilBlock[0] = i
         if (tilesTop[0].index == 45){
             break
-        }
-        if (tilesTop[0].index == 46){
+        } else if (tilesTop[0].index == 46) {
             loot(tilesTop)
             break
         }
     }
     for(var i = 1; i <= bonusBombRange+1; i++){
         tilesRight = map.getTilesWithinWorldXY(bomb.x + (TILE_SIZE / 2) + TILE_SIZE * (i-1), bomb.y - (TILE_SIZE / 2), TILE_SIZE, TILE_SIZE)
+        bonusUntilBlock[1] = i
         if (tilesRight[0].index == 45){
             break
-        }
-        if (tilesRight[0].index == 46){
+        } else if (tilesRight[0].index == 46) {
             loot(tilesRight)
             break
         }
     }
     for(var i = 1; i <= bonusBombRange+1; i++){
         tilesBot = map.getTilesWithinWorldXY(bomb.x - (TILE_SIZE / 2), bomb.y + (TILE_SIZE / 2)  + TILE_SIZE * (i-1), TILE_SIZE, TILE_SIZE)
+        bonusUntilBlock[2] = i
         if (tilesBot[0].index == 45){
             break
-        }
-        if (tilesBot[0].index == 46){
+        } else if (tilesBot[0].index == 46) {
             loot(tilesBot)
             break
         }
     }
     for(var i = 1; i <= bonusBombRange+1; i++){
         tilesLeft = map.getTilesWithinWorldXY(bomb.x - (TILE_SIZE * i + (TILE_SIZE / 2)), bomb.y - (TILE_SIZE / 2), TILE_SIZE, TILE_SIZE)
+        bonusUntilBlock[3] = i
         if (tilesLeft[0].index == 45){
             break
-        }
-        if (tilesLeft[0].index == 46){
+        } else if (tilesLeft[0].index == 46) {
             loot(tilesLeft)
             break
         }
     }
 
+    console.log(bonusUntilBlock, bonusBombRange)
 
-    isDeadP1 = playerIsDead(player1, bomb, bonusBombRange)
-    isDeadP2 = playerIsDead(player2, bomb, bonusBombRange)
+
+    isDeadP1 = playerIsDead(player1, bomb, bonusUntilBlock, bonusBombRange)
+    isDeadP2 = playerIsDead(player2, bomb, bonusUntilBlock, bonusBombRange)
     
     if(playerType == 'player1') {
         p1CompteurBombe -= 1
